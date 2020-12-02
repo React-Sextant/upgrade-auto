a4051976874483c40d725cf31f08ff8d0e1d4879
罗坤 <1498710037@qq.com>
2020-12-02 17:15:26 +0800

    Update upgrade-components.js

--- upgrade-components.js
+++ upgrade-components.js
@@ -30,7 +30,7 @@ function travel(dir, callback, finish) {
                                     const reg = `(import.*)(${component}\s*,|${component},|${component})(.*react-native["|'].*)`;
                                     if(new RegExp(reg).test(data)){
                                         _data = (_data||data).replace(new RegExp(reg),`$1$3\nimport {${component} } from "${package}";`);
-                                        _install[pathname] ? _install[pathname].push("\""+package+"\"") : _install[pathname] = ["\""+package+"\"","\""+package+"\""];
+                                        _install[pathname] ? _install[pathname].push("\""+package+"\"") : _install[pathname] = ["\""+package+"\""];
                                     }
                                 }
                                 if(_data){
