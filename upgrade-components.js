10e8777f60acf49c46b4e7eaa4da1e4e5670808f
罗坤 <1498710037@qq.com>
2020-12-04 15:15:54 +0800

    rewrite upgrade-components.js

--- upgrade-components.js
+++ upgrade-components.js
@@ -3,12 +3,55 @@ const fs = require("fs");
 // UNDO: Ignore .gitignore file
 const dir = './';
 
-let upgradeDependencies = {};
+let upgradeDependencies = [];
 try{
-    upgradeDependencies = require("../../../package.json").upgradeDependencies
+    let _upgradeDependencies = require("../../../package.json").upgradeDependencies
+    for (const component in _upgradeDependencies) {
+        let package = _upgradeDependencies[component]||"";
+        let oldPackage = "react-native";
+
+        let replaceValue = "";
+
+        /**
+         * {
+         *   "NavigationActions": "@react-navigation/compat > react-navigation"
+         * }
+         * **/
+        if(package.indexOf(">") > -1){
+            const _arr = package.split(">");
+            package = _arr[0];
+            oldPackage = _arr[1];
+        }
+        const reg = `(import.*?)(${component}.*?[,])(.*?${oldPackage.trim()}["|'])`;
+
+        /**
+         * {
+         *   "ART": "import * as ART from '@react-native-community/art'"
+         * }
+         * **/
+        if(new RegExp(/'(.+)'|"(.+)"/).test(package)){
+            replaceValue = `$1$3\n${package.trim()};`;
+            package = new RegExp(/'(.+)'|"(.+)"/).exec(package)[1];
+        }else {
+            /**
+             * {
+             *   "WebView": "react-native-webview"
+             * }
+             * **/
+            replaceValue = `$1$3\nimport ${component} from "${package}";`
+        }
+
+        upgradeDependencies.push({
+            component,
+            package,
+            oldPackage,
+            replaceValue,
+            reg
+        });
+    }
 }catch (e) {
 }
-console.log("🔭 Start upgrade ... \n",upgradeDependencies);
+console.log("🔭 Start upgrade ... \n");
 
 function travel(dir, callback, finish) {
     fs.readdir(dir, function (err, files) {
@@ -20,51 +63,21 @@ function travel(dir, callback, finish) {
                         travel(pathname,callback,finish)
                     }else {
                         const extname = path.extname(pathname);
+
                         if(extname === ".js" || extname === ".ts" || extname === ".tsx"){
                             fs.readFile(pathname, "utf8", function(err, data) {
-
                                 let _data = "";
                                 let _install = {};
-                                for (const component in upgradeDependencies) {
-                                    let package = upgradeDependencies[component]||"";
-                                    let oldPackage = "react-native";
-
-                                    /**
-                                     * {
-                                     *   "NavigationActions": "@react-navigation/compat > react-navigation"
-                                     * }
-                                     * **/
-                                    if(package.indexOf(">") > -1){
-                                        const _arr = package.split(">");
-                                        package = _arr[0];
-                                        oldPackage = _arr[1];
+                                upgradeDependencies.forEach(b=>{
+                                    if(new RegExp(b.reg).test(data)){
+                                        _data = (_data||data).replace(new RegExp(b.reg),b.replaceValue);
+                                        _install[pathname] ? _install[pathname].push("\""+b.package+"\"") : _install[pathname] = ["\""+b.package+"\""];
                                     }
-
-                                    const reg = `(import.*)(${component}.*?[,])(.*${oldPackage.trim()}["|'].*)`;
-                                    if(new RegExp(reg).test(data)){
-                                        /**
-                                         * {
-                                         *   "ART": "import * as ART from '@react-native-community/art'"
-                                         * }
-                                         * **/
-                                        if(new RegExp(/'(.+)'|"(.+)"/).test(package)){
-                                            _data = (_data||data).replace(new RegExp(reg),`$1$3\n${package.trim()};`);
-                                            package = new RegExp(/'(.+)'|"(.+)"/).exec(package)[1];
-                                        }else {
-                                            /**
-                                             * {
-                                             *   "WebView": "react-native-webview"
-                                             * }
-                                             * **/
-                                            _data = (_data||data).replace(new RegExp(reg),`$1$3\nimport ${component} from "${package}";`);
-                                        }
-                                        _install[pathname] ? _install[pathname].push("\""+package+"\"") : _install[pathname] = ["\""+package+"\""];
-                                    }
-                                }
+                                })
                                 if(_data){
                                     fs.writeFile(pathname, _data,callback.bind(this,path.resolve(pathname),_install[pathname].join(" 、 ")));
                                 }
-                            });
+                            })
                         }
                     }
                 });
